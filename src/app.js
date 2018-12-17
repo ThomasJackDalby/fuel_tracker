6098d93ff2481ebf10d4c9098a52214e0d971b30
Tom Dalby <thomasjackdalby@gmail.com>
2018-12-17 18:02:21 +0000

    experimenting with static host

--- src/app.js
+++ src/app.js
@@ -21,8 +21,7 @@ app.use(function (req, res, next) {
 });
 
 logger.info("Launching application from "+__dirname)
-app.use('/', express.static(path.join(__dirname, 'public')))
-app.use(express.directory(__dirname + '/public'));
+app.use('', express.static(path.join(__dirname, 'public')))
 app.use(passport.initialize());
 app.use('/api', router);
 app.use(cookieParser())
