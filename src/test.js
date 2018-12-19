cf01ecdaf41883e6a2aa5414e4c76dd17307e3b5
Tom Dalby <thomasjackdalby@gmail.com>
2018-12-19 21:47:23 +0000

    added test file

---
+++ src/test.js
@@ -0,0 +1,7 @@
+console.log("Testing");
+console.log("-----------");
+console.log(process.env.DATABASE_HOST);
+console.log(process.env.DATABASE_USER);
+console.log(process.env.DATABASE_PASSWORD);
+console.log(process.env.DATABASE_NAME);
+console.log("-----------");
